fc5a1d613ef42a29a2e1000d10168bf64f388ae9
Gautham1231 <gautham.sevugan@gmail.com>
2025-03-07 13:34:15 -0500

    Completed almost all of plaid and dwolla integration

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,22 @@
 import HeaderBox from '@/components/HeaderBox';
 import RightSideBar from '@/components/RightSideBar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page }}: SearchParamProps) => {
     const loggedIn = await getLoggedInUser();
+    const accounts = await getAccounts({ userId: loggedIn.$id });
+
+    if (!accounts) return;
+
+    const accountsData = accounts?.data;
+
+    const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+    const account = await getAccount({ appwriteItemId });
+    
     return (
         <section className="home">
             <div className="home-content">
@@ -13,14 +24,14 @@ const Home = async () => {
                     <HeaderBox
                         type="greeting"
                         title="Welcome"
-                        user={loggedIn?.name || 'Guest'}
+                        user={loggedIn?.firstName || 'Guest'}
                         subtext="Access and Manage your account and transactions"
                     />
 
                     <TotalBalanceBox
-                        accounts={[]}
-                        totalBanks={1}
-                        totalCurrentBalance={9000.12} 
+                        accounts={accountsData}
+                        totalBanks={accounts?.totalBanks}
+                        totalCurrentBalance={accounts?.totalCurrentBalance} 
                     />
 
                 </header>
@@ -30,8 +41,8 @@ const Home = async () => {
 
             <RightSideBar 
                 user={loggedIn}
-                transactions={[]}
-                banks={[{ currentBalance: 1450.00},{ currentBalance: 1542.23}]}
+                transactions={accounts?.transactions}
+                banks={accountsData?.slice(0, 2)}
             />
         </section>
     )
